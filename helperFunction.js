fd690d0e8b95362288f72871d743e16cd364cfd1
anuja-uk <anujap.uk@gmail.com>
2019-09-25 23:58:04 +0530

    my project

---
+++ helperFunction.js
@@ -0,0 +1,130 @@
+var selectedState,errorCode;
+
+
+function getProcessingPage(data){	
+
+	// data.length = 1 means processing state is not checked
+	if(data.length == 1){
+		// check which state is selected and call respective function 
+		if(data[0].state == 'success'){
+			success();
+		} else if(data[0].state == 'error'){		
+			error(data[0]);
+		}
+	} else {
+		processing(data);
+	}	
+};
+
+function processing(data){
+	console.log("processing... Please wait for 2 seconds! ");
+	// function delay of 2 seconds, then execute nect state
+	setTimeout(function(){
+		data.forEach(function(element,index) {
+			// check which state is selected with processing and call respective function 
+			if(element.state == "success"){
+				success();
+			} else if(element.state == "error"){
+				
+				error(data[index]);
+			}
+		});}, 2000);
+}
+
+function success(){
+
+	var successObj = {
+		title: 'Order complete', 
+		message: null
+	};
+	console.log(successObj);
+}
+
+function error(data){
+
+	var errorMessage;
+	data.errorCode.forEach(function(element,index) {				
+				if(element == errorCode){
+					errorMessage = checkErrorCode(data.errorCode[index])
+				} else if(!errorCode){
+					errorMessage = checkErrorCode();
+				}
+		});		
+		console.log(errorMessage);	
+
+}
+
+function checkErrorCode(code){
+	 
+	switch(code){
+		case 'NO_STOCK':
+				return { title: 'Error page', message: 'No stock has been found' };
+			break;
+		case 'INCORRECT_DETAILS':
+				return { title: 'Error page', message: 'Incorrect details have been entered' };
+			break;
+		case 'null':
+				return { title: 'Error page', message: null };
+			break;
+		case 'undefined':
+				return { title: 'Error page', message: null };
+			break;
+		default:
+				return { title: 'Error page', message: null };
+	}
+
+}; 
+
+function stateSelection(){
+	state = document.getElementsByName('successOrerror');
+	// get seleced state 
+	for(i = 0; i < state.length; i++) { 
+		if(state[i].checked) {			
+			selectedState = state[i].value;			
+		} 
+	} 
+	// check whether selected state is error
+	 selectedState == "error" ? showErrorCode.style.display = "block" : showErrorCode.style.display = "none"; 
+}
+
+function errorCodeSelection(){
+	errorCodes= document.getElementsByName("errorCode");
+	// get seleced state 
+	for(i = 0; i < errorCodes.length; i++) { 
+		if(errorCodes[i].checked) {			
+			errorCode = errorCodes[i].value;			
+		} 
+	} 
+	
+}
+
+function init(){
+
+	var data = [{
+		state: 'processing'
+	},{
+		state: 'success'
+	},{
+		state: 'error',
+		errorCode: ['NO_STOCK','INCORRECT_DETAILS','null','undefined']
+	}];
+	var selectedData = [],isProcessing;
+	
+	//is processing state selected
+	isProcessing =  document.getElementById('processing').checked;
+	
+    //Check state is selected or not
+	if(selectedState){
+		//create new array with selected states 
+		data.forEach(function(element,index) {
+			if(isProcessing && element.state == "processing"){
+				selectedData.push(data[index]);
+			} else if(element.state == selectedState){
+				selectedData.push(data[index]);
+			}
+		});	
+		// call function for output
+		getProcessingPage(selectedData);
+	} else 
+		console.log("Please select other state along with processing state!");
+}
